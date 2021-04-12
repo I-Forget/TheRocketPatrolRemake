fc0becd8cd3d6e9ea52f879992c18ed68fad0b4a
bdvictor <bdvictor@ucsc.edu>
2021-04-12 10:44:51 -0700

    Play.js shows up from transition in menu

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,6 +15,13 @@ class Play extends Phaser.Scene {
             game.config.width,
             borderUISize * 2,
             0x00FF00,
-            );
+            ).setOrigin(0,0);
+
+        //white borders when they appear lol
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    
     }
 }
